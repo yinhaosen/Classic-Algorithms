03e2e928d112688db169477d185a18d446021730
Haosen Yin <yinhaosen@foxmail.com>
2016-06-30 22:41:21 +0800

    Update BSearch.c

--- BSearch.c
+++ BSearch.c
@@ -40,11 +40,33 @@ int BSearch(int list[],int input,int low,int high)
         return -1;      //查找失败
     else if(input == list[mid])
         return mid;
-    else if(input<list[mid])            //x在下半区查找
+    else if(input < list[mid])            //x在左半区查找
         return BSearch(list,input,low,mid-1);
-    else                                //x在上半区查找
+    else                                //x在右半区查找
         return BSearch(list,input,mid+1,high);
         
         
+}
+
+
+/* 利用非递归的方法进行二分查找 */
+int BSearch2(int list[],int input,int low,int high)
+{
+    int mid;
     
+    while(low <= high)
+    {
+        mid=(low+high)/2;
+        
+        if(input == list[mid])
+            return mid;
+        if(input < list[mid])
+            high=mid-1;
+        else
+            low=mid+1;
+            
+    }
+    
+    return -1;      /* 查找失败 */
+
 }
